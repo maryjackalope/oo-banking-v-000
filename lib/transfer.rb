4fa6b9c904dd3393087cb3f642e6aedb18d2cbef
Mary Jack <maryjackinthebox@gmail.com>
2018-05-22 19:28:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,9 +3,8 @@ class Transfer
   attr_accessor :amount, :sender, :receiver, :status
   
   def initialize(name)
-    @name = name
-    @transfer
-    @status = pending
+    @status = "pending"
+    
   end 
   
  
